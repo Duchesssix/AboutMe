7aaee4a08174665b8776ed9646ee34ef048a8d06
YOUR_PUBLIC_NAME <YOUR_PUBLIC@GIT_EMAIL.COM>
2017-08-29 17:54:20 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args){
+        //Attempt #2
+        System.out.println("Hello! My name is Dawn!");
+        System.out.println("I like programming and take classes just for fun.");
+        System.out.println("I am currently a data analyst.");
+        System.out.println("My dream job would be as a programmer.");
+        System.out.println("Unfortunately, my Bachelor's and Master's are in Education and not Computer Science.");
+    }
+}
